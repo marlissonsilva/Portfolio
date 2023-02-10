1977874c42a2de5f3d66b19cc6054f4a4278b462
Marlisson Silva <marlyssonsilva005@gmail.com>
2023-02-10 08:51:00 -0300

    website style changes

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,6 @@
 const dataModal = [
     {
-
+        id: 1,
         title: 'hora do dia',
         img: 'https://github.com/marlissonsilva/images/blob/main/hora-do-dia.png?raw=true',
         desc: '    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore alias accusantium excepturi id nobis velit! Reiciendis obcaecati quos, distinctio unde enim expedita. At unde modi non cum obcaecati nulla dolorem?',
@@ -9,16 +9,16 @@ const dataModal = [
         repositorio: 'https://github.com/marlissonsilva/hora-do-dia',
     },
     {
-
+        id: 2,
         title: 'arte e sorriso',
-        img: 'https://github.com/marlissonsilva/images/blob/main/arte-sorriso.png?raw=true',
+        img: './images/arte-video.mp4',
         desc: '    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore alias accusantium excepturi id nobis velit! Reiciendis obcaecati quos, distinctio unde enim expedita. At unde modi non cum obcaecati nulla dolorem?',
         techs: 'html, css, js',
         projeto: 'https://marlissonsilva.github.io/arte-sorriso/',
         repositorio: 'https://github.com/marlissonsilva/arte-sorriso',
     },
     {
-
+        id: 3,
         title: 'Collor Flipper',
         img: 'https://github.com/marlissonsilva/images/blob/main/collor-flipper.png?raw=true',
         desc: '    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore alias accusantium excepturi id nobis velit! Reiciendis obcaecati quos, distinctio unde enim expedita. At unde modi non cum obcaecati nulla dolorem?',
@@ -28,25 +28,25 @@ const dataModal = [
 
     },
     {
-
+        id: 4,
         title: 'Order summary card',
         img: 'https://github.com/marlissonsilva/images/blob/main/oder-summary-card.png?raw=true',
         desc: '    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore alias accusantium excepturi id nobis velit! Reiciendis obcaecati quos, distinctio unde enim expedita. At unde modi non cum obcaecati nulla dolorem?',
         techs: 'html, css, js',
-        projeto: 'https://marlissonsilva.github.io/order-summary-component/',
-        repositorio: 'https://github.com/marlissonsilva/order-summary-component',
+        projeto: 'https://marlissonsilva.github.io/sunnyside-agency-landing-page-main/',
+        repositorio: 'https://github.com/marlissonsilva/sunnyside-agency-landing-page-main',
     },
     {
-
-        title: 'column preview card component',
-        img: 'https://github.com/marlissonsilva/images/blob/main/column-preview-card.png?raw=true',
+        id: 5,
+        title: 'rest-countries-api-with-color-theme-switcher',
+        img: 'https://github.com/marlissonsilva/rest-countries-api-with-color-theme-switcher-master/blob/main/img/preview-project.png?raw=true',
         desc: '    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore alias accusantium excepturi id nobis velit! Reiciendis obcaecati quos, distinctio unde enim expedita. At unde modi non cum obcaecati nulla dolorem?',
-        techs: 'html, css, js',
-        projeto: 'https://marlissonsilva.github.io/3column-preview-card-component-main/',
-        repositorio: 'https://github.com/marlissonsilva/3column-preview-card-component-main',
+        techs: 'html, css, js, api',
+        projeto: 'https://marlissonsilva.github.io/rest-countries-api-with-color-theme-switcher-master/',
+        repositorio: 'https://github.com/marlissonsilva/rest-countries-api-with-color-theme-switcher-master',
     },
     {
-
+        id: 6,
         title: 'IMC',
         img: 'https://github.com/marlissonsilva/images/blob/main/imc.png?raw=true',
         desc: '    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore alias accusantium excepturi id nobis velit! Reiciendis obcaecati quos, distinctio unde enim expedita. At unde modi non cum obcaecati nulla dolorem?',
@@ -55,34 +55,6 @@ const dataModal = [
         repositorio: 'https://github.com/marlissonsilva/imc',
     }
     // {
-
-    //     title: 'cards car preview 7',
-    //     img: 'https://github.com/marlissonsilva/portfolio/blob/main/images/Captura%20de%20tela%202022-12-01%20080231.png?raw=true',
-    //     desc: '    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore alias accusantium excepturi id nobis velit! Reiciendis obcaecati quos, distinctio unde enim expedita. At unde modi non cum obcaecati nulla dolorem?',
-    //     techs: 'html, css, js',
-    //     projeto: 'https://marlissonsilva.github.io/hora-do-dia/',
-    //     repositorio: 'https://github.com/marlissonsilva/hora-do-dia',
-
-    // },
-    //     {
-
-    //         title: 'Card Order Sumary 8',
-    //         img: 'https://github.com/marlissonsilva/portfolio/blob/main/images/Captura%20de%20tela%202022-12-05%20072353.png?raw=true',
-    //         desc: '    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore alias accusantium excepturi id nobis velit! Reiciendis obcaecati quos, distinctio unde enim expedita. At unde modi non cum obcaecati nulla dolorem?',
-    //         techs: 'html, css, js',
-    //         projeto: 'https://marlissonsilva.github.io/hora-do-dia/',
-    //         repositorio: 'https://github.com/marlissonsilva/hora-do-dia',
-    //     }
-    //     ,
-    //     {
-
-    //         title: 'Card Order Sumary 9',
-    //         img: 'https://github.com/marlissonsilva/portfolio/blob/main/images/Captura%20de%20tela%202022-12-01%20080231.png?raw=true',
-    //         desc: '    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore alias accusantium excepturi id nobis velit! Reiciendis obcaecati quos, distinctio unde enim expedita. At unde modi non cum obcaecati nulla dolorem?',
-    //         techs: 'html, css, js',
-    //         projeto: 'https://marlissonsilva.github.io/hora-do-dia/',
-    //         repositorio: 'https://github.com/marlissonsilva/hora-do-dia',
-    //     }
 ]
 
 
@@ -96,6 +68,12 @@ const cards = document.querySelectorAll('[data-index]');
 const fade = document.querySelector('#fade');
 const modal = document.querySelector('#modal');
 const closeModal = document.querySelector('.close-button')
+const withoutJs = document.querySelectorAll('.without-js')
+
+
+withoutJs.forEach((element) => {
+    element.style.display = 'none'
+})
 
 cards.forEach(card => {
     card.addEventListener('click', () => {
@@ -119,8 +97,8 @@ cards.forEach(card => {
 cards.forEach(card => {
     card.addEventListener('click', () => {
         const index = card.getAttribute('data-index');
-        console.log(`${index}`);
-        console.log(dataModal[index])
+        // console.log(`${index}`);
+        // console.log(dataModal[index])
         showModal(index);
     });
 });
@@ -129,6 +107,7 @@ cards.forEach(card => {
 
 function showModal(index) {
     const item = dataModal[index]
+    console.log('id ' + item.id)
     img.src = item.img;
     title.textContent = item.title;
     desc.textContent = item.desc;
--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 // MENU MOBILE
 
+
+
 const menuMobile = document.querySelector('.menu-mobile')
 function showMenu(event) {
     if (event.type === 'touchstart') event.preventDefault()
     const nav = document.querySelector('#nav')
     nav.classList.toggle('active')
+    if (nav.classList.contains('active')) {
+        document.body.style.overflow = 'hidden'
+    } else {
+        document.body.style.overflow = 'visible'
+    }
 }
 
 menuMobile.addEventListener('touchstart', showMenu)
@@ -13,18 +20,41 @@ menuMobile.addEventListener('click', showMenu)
 // HIDDEN MENU
 let menuItens = document.querySelectorAll('.menu a');
 menuItens.forEach(item => {
-    item.addEventListener('click', hiddenMenu)
+    item.addEventListener('click', hiddenMenu);
+    // item.addEventListener('click', scrollClick);
+    item.addEventListener('click', activeLink);
 })
+var sections = document.querySelectorAll('.section');
+var menu = document.querySelector('.menu');
+var menuItems = menu.querySelectorAll('a');
+
+window.addEventListener('scroll', function () {
+    var scrollPos = window.scrollY + menu.offsetHeight;
+    for (var i = 0; i < sections.length; i++) {
+        var section = sections[i];
+
+        if (scrollPos > section.offsetTop && scrollPos < section.offsetTop + section.offsetHeight) {
+            menuItems[i].classList.add('active-link-menu');
+        }
+        else {
+            menuItems[i].classList.remove('active-link-menu');
+        }
+    }
+});
+
+function activeLink() {
+    menuItens.forEach((item) => {
+        item.classList.remove('active-link-menu')
+        this.classList.add('active-link-menu')
+    });
+}
 
 function hiddenMenu(event) {
     if (event.type === 'touchstart') event.preventDefault()
     const nav = document.querySelector('#nav')
     nav.classList.remove('active')
+    document.body.style.overflow = 'visible'
 }
-
-menuItens.forEach(item => {
-    item.addEventListener('click', scrollClick)
-});
 
 function scrollClick(event) {
     event.preventDefault()
@@ -43,14 +73,17 @@ function scrollClick(event) {
 const root = document.documentElement;
 root.className += "js";
 function animeScroll() {
+
     const target = document.querySelectorAll(".anime");
     target.forEach((element) => {
         let contentPosition = element.getBoundingClientRect().top;
         let screenPosition = window.innerHeight * 0.65;
         if (contentPosition < screenPosition) {
             element.classList.add("animate");
+
         } else {
             element.classList.remove("animate");
+
         }
     });
 }
@@ -59,6 +92,16 @@ animeScroll();
 
 window.addEventListener("scroll", function () {
     animeScroll();
+
 });
+
+
+// VARIAVEIS
+
+
+// EVENTOS
+
+
+// FUNÇÕES
 
 
