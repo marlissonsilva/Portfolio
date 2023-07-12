ac44e602902e6fc8edaa8ea7897c3170ea114981
Marlisson Silva <marlyssonsilva005@gmail.com>
2023-07-12 08:33:37 -0300

    production for  version

--- js/modal.js
+++ js/modal.js
@@ -1,89 +1,73 @@
 const dataModal = [
   {
-    id: 1,
+    id: 0,
     title: "página de captura",
-    image: "../images/weda-mockup.webp",
+    image: "../images/page-capute-mockup.webp",
     description: [
       "Página de cadastro para receber conteúdos sobre programação, com os inputs de nome, email e com as opções de estados, foi feito a validação dos campos de input, a validação de email por meio de uma expressão regular, mostra mensagem de preenchimento incorreto e por fim abre um modal retornando a confirmação do cadastro com os dados inseridos pelo usuário.",
     ],
     techs: [
-      'fa-brands fa-html5"',
-      'fa-brands fa-css3-alt"',
-      'fa-brands fa-square-js"',
+      '<i class="fa-brands fa-html5"></i>',
+      '<i class="fa-brands fa-css3-alt"></i>',
+      '<i class="fa-brands fa-square-js"></i>',
     ],
     project: "https://marlissonsilva.github.io/capture-page/",
     repository: "https://github.com/marlissonsilva/capture-page",
   },
   {
-    id: 2,
-    title: "sunnyside-agency",
-    image: "../images/sunnyside-mockup.webp",
+    id: 1,
+    title: "Quiz",
+    image: "../images/quiz.png",
     description: [
-      "    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore alias accusantium excepturi id nobis velit! Reiciendis obcaecati quos, distinctio unde enim expedita. At unde modi non cum obcaecati nulla dolorem?",
+      "É um quiz sobre os times de futebol brasilero, para testar seus conhecimentos sobre o nosso futebol. Onde o usuário tem 10 questões com 4 alternativas de respostas para cada. E ao fim do questionário após o envio vem a animação de scroll até o topo para visualização da pontuação, tendo uma animação a mais quando é obtido a pontuação máxima.",
     ],
     techs: [
-      'fa-brands fa-html5"',
-      'fa-brands fa-css3-alt"',
-      'fa-brands fa-square-js"',
+      '<i class="fa-brands fa-html5"></i>',
+      '<i class="fa-brands fa-square-js"></i>',
+      '<i class="fa-brands fa-bootstrap"></i>',
     ],
-    project:
-      "https://marlissonsilva.github.io/sunnyside-agency-landing-page-main/",
-    repository:
-      "https://github.com/marlissonsilva/sunnyside-agency-landing-page-main",
+    project: "https://marlissonsilva.github.io/Quiz/",
+    repository: "https://github.com/marlissonsilva/Quiz",
   },
 
   {
-    id: 3,
-    title: "arte e sorriso",
-    image: "./images/weda-mockup.webp",
+    id: 2,
+    title: "Consumindo API do GIFHY",
+    image: "../images/gifhy.png",
     description: [
-      "Site feito para uma clínica ondontologica (fictício), na verdade foi feito baseado em uma clinica na qual ja frequentei, é um site simples de apresentação de todas as especialidades e seus profissionais. As principais funções do site são:",
-      "Contar um pouco sobre a clínica",
-      "Mostar os planos conveniados",
-      "Cards com as especialidades, e um modal com mais informações de cada especialidade",
-      "Cards com apresentando os dentistas",
-      "Seção de comentários dos clientes.",
-      "Localização",
-      "Links para redes sociais",
+      "Aplicação feita como desafio do curso de javascript, onde é feito o consumo da API GIFHY, retornando um GIF corespondente ao termo digitado no input",
     ],
     techs: [
-      'fa-brands fa-html5"',
-      'fa-brands fa-css3-alt"',
-      'fa-brands fa-square-js"',
+      '<i class="fa-brands fa-html5"></i>',
+      '<i class="fa-brands fa-css3-alt"></i>',
+      '<i class="fa-brands fa-square-js"></i>',
+    ],
+    project: "https://fetch-api-gifhy.netlify.app/",
+    repository: "https://github.com/marlissonsilva/fetch-gif-in-gifhy-api",
+  },
+  {
+    id: 3,
+    title: "arte & sorriso",
+    image: "../images/mockup-arte1.webp",
+    description: [
+      "Site feito para uma clínica ondontologica (fictício), na verdade foi feito baseado em uma clinica na qual ja frequentei, é um site simples de apresentação de todas as especialidades e seus profissionais. As principais funções do site são:,Contar um pouco sobre a clínica,Mostar os planos conveniados,Cards com as especialidades, e um modal com mais informações de cada especialidade,Cards com apresentando os dentistas,Seção de comentários dos clientes.,Localização,Links para redes sociais",
+    ],
+    techs: [
+      '<i class="fa-brands fa-html5"></i>',
+      '<i class="fa-brands fa-css3-alt"></i>',
+      '<i class="fa-brands fa-square-js"></i>',
     ],
     project: "https://marlissonsilva.github.io/arte-sorriso/",
     repository: "https://github.com/marlissonsilva/arte-sorriso",
   },
-  {
-    id: 4,
-    title: "escola weda",
-    image: "../images/sunnyside-mockup.webp",
-    description: [
-      "Site de uma academia de lutas, primeiro projeto que foi ao ar na web, foi feito para um amigo. possui tais funcionalidaes como: ",
-
-      "Apresentar a academia",
-      "Mostar as modalidades disponiveis",
-      "Botão de contato de acordo com cada modalidade, já com uma mensagem pré-definida para o WhatsApp",
-      "Localização ",
-      "Carrossel de imagens das dependencias da academia, seus alunos e professores",
-      "Link de direcionamento para a rede social",
-    ],
-    techs: [
-      'fa-brands fa-html5"',
-      'fa-brands fa-css3-alt"',
-      'fa-brands fa-square-js"',
-    ],
-    project: "https://preview-weda.netlify.app/",
-    repository: "https://github.com/marlissonsilva",
-  },
 ];
 
-const image = document.querySelector("#img-person");
-const title = document.querySelector("#title");
-const description = document.getElementById("descripition");
-const techs = document.querySelector("#techs");
-const projeto = document.querySelector("#site");
-const repositorio = document.querySelector("#repositorio");
+const image = document.querySelector(".img-person");
+const title = document.querySelector(".title");
+const desc = document.querySelector(".desc");
+const techs = document.querySelector(".technologies-used");
+const projeto = document.querySelector(".site");
+const repositorio = document.querySelector(".repositorio");
 const cardsIndex = document.querySelectorAll("[data-index]");
 const wrappers = document.querySelectorAll(".wrapper-card");
 const containerCards = document.querySelector(".projects");
@@ -97,7 +81,6 @@ wrappers.forEach((wrapper) => {
   wrapper.style.display = "none";
 });
 
-console.log(containerCards);
 containerCards.children[0].insertAdjacentElement("afterend", paragraph);
 
 const openModal = (event) => {
@@ -132,7 +115,7 @@ const showModal = (index) => {
   image.src = item.image;
   title.textContent = item.title;
   desc.innerHTML = item.description;
-  techs.innerHTML = `<i class="${item.techs}"></i>`;
+  techs.innerHTML = item.techs.join(" ");
   projeto.href = item.project;
   repositorio.href = item.repository;
 };
